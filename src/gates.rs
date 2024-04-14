da73dcb0009d1d25f88958069d99ad168c9fe1b5
Luke McNeil <lukemcneil61@gmail.com>
2024-04-14 00:13:53 -0500

    grow worker turning into fighter

--- src/gates.rs
+++ src/gates.rs
@@ -1,4 +1,5 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, sprite::Anchor};
+use bevy_inspector_egui::egui::lerp;
 use bevy_rapier2d::prelude::*;
 
 use crate::{
@@ -14,6 +15,7 @@ pub struct GatePlugin;
 
 const GATE_WIDTH: f32 = WORKER_RENDER_WIDTH * 1.5;
 const GATE_HEIGHT: f32 = WORKER_RENDER_HEIGHT * 1.5;
+const GATE_TIME: f32 = 1.0;
 
 impl Plugin for GatePlugin {
     fn build(&self, app: &mut App) {
@@ -90,8 +92,11 @@ struct GateTimer {
 }
 
 fn check_worker_gate_collisions(
-    players_with_berries: Query<(Has<GateTimer>, Has<Berry>, Has<Queen>, &Team), With<Player>>,
-    mut gates: Query<(Option<&Team>, &mut Sprite), With<Gate>>,
+    mut players_with_berries: Query<
+        (Has<GateTimer>, Has<Berry>, Has<Queen>, &Team, &mut Sprite),
+        With<Player>,
+    >,
+    mut gates: Query<(Option<&Team>, &mut Sprite), (With<Gate>, Without<Player>)>,
     mut collision_events: EventReader<CollisionEvent>,
     mut commands: Commands,
 ) {
@@ -100,7 +105,7 @@ fn check_worker_gate_collisions(
             CollisionEvent::Started(entity1, entity2, _) => {
                 for (gate_entity, player_entity) in [(entity1, entity2), (entity2, entity1)] {
                     if let Ok((maybe_gate_team, mut gate_sprite)) = gates.get_mut(*gate_entity) {
-                        if let Ok((player_has_gate_timer, player_has_berry, is_queen, team)) =
+                        if let Ok((player_has_gate_timer, player_has_berry, is_queen, team, _)) =
                             players_with_berries.get(*player_entity)
                         {
                             if is_queen {
@@ -114,7 +119,7 @@ fn check_worker_gate_collisions(
                             }
                             if !player_has_gate_timer && player_has_berry {
                                 commands.entity(*player_entity).insert(GateTimer {
-                                    timer: Timer::from_seconds(1.0, TimerMode::Once),
+                                    timer: Timer::from_seconds(GATE_TIME, TimerMode::Once),
                                 });
                             }
                         }
@@ -124,11 +129,18 @@ fn check_worker_gate_collisions(
             CollisionEvent::Stopped(entity1, entity2, _) => {
                 for (gate_entity, player_entity) in [(entity1, entity2), (entity2, entity1)] {
                     if gates.get(*gate_entity).is_ok() {
-                        if let Ok((player_has_gate_timer, player_has_berry, _, _)) =
-                            players_with_berries.get(*player_entity)
+                        if let Ok((player_has_gate_timer, player_has_berry, _, _, mut sprite)) =
+                            players_with_berries.get_mut(*player_entity)
                         {
                             if player_has_berry && player_has_gate_timer {
                                 commands.entity(*player_entity).remove::<GateTimer>();
+                                let (player_width, player_height) =
+                                    (WORKER_RENDER_WIDTH, WORKER_RENDER_HEIGHT);
+                                sprite.custom_size = Some(Vec2 {
+                                    x: player_width,
+                                    y: player_height,
+                                });
+                                sprite.anchor = Anchor::Center;
                             }
                         }
                     }
@@ -154,6 +166,7 @@ fn progress_gate_timers(
                 x: player_width,
                 y: player_height,
             });
+            sprite.anchor = Anchor::Center;
             transform.translation.y += (QUEEN_RENDER_HEIGHT - WORKER_RENDER_HEIGHT) / 2.0;
             commands
                 .entity(entity)
@@ -184,6 +197,20 @@ fn progress_gate_timers(
                         Crown,
                     ));
                 });
+        } else {
+            // grow sprite
+            let percent_done = gate_timer.timer.elapsed_secs() / GATE_TIME;
+            let (player_width, player_height) = (
+                lerp(WORKER_RENDER_WIDTH..=QUEEN_RENDER_WIDTH, percent_done),
+                lerp(WORKER_RENDER_HEIGHT..=QUEEN_RENDER_HEIGHT, percent_done),
+            );
+            sprite.custom_size = Some(Vec2 {
+                x: player_width,
+                y: player_height,
+            });
+            sprite.anchor = Anchor::Custom(
+                Vec2::new(0.0, -(player_height - WORKER_RENDER_HEIGHT) / 2.0) / player_height,
+            )
         }
     }
 }
